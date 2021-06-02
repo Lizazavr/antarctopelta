4817ad92d63e97951ea07dc67732b5204b95c13c
Lizazavr <78729103+Lizazavr@users.noreply.github.com>
2021-06-02 09:15:49 +0300

    Update create_text.cpp

--- app/create_text.cpp
+++ app/create_text.cpp
@@ -8,25 +8,5 @@ text create_text()
 {
     text txt;
 
-    if ((txt = (_list *) malloc(sizeof(_list))) == NULL)
-    {
-        fprintf(stderr, "Not enough memory!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    txt = new _list;
-
-    if ((txt->cursor = (_crsr *) malloc(sizeof(_crsr))) == NULL)
-    {
-        fprintf(stderr, "Not enough memory!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    txt->myList = std::list<std::string>();
-
-
-    txt->cursor->line = txt->myList.end();
-    txt->cursor->position = 0;
-
     return txt;
 }
