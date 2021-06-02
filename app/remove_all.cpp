8287e90ae3e9ff8e4e0a281b5f30de3ee4d69efc
Lizazavr <78729103+Lizazavr@users.noreply.github.com>
2021-06-02 07:44:48 +0300

    Update remove_all.cpp

--- app/remove_all.cpp
+++ app/remove_all.cpp
@@ -2,11 +2,11 @@
 
 void remove_all(text txt)
 {
-    /* Проверяем, имеется ли текст */
+    /* Проверяем, имеется ли текст 
     if (txt == NULL) {
         fprintf(stderr, "The text doesn't exist!\n");
         return;
-    }
+    }*/
 
     /* Если список изначально пуст, делать ничего не надо */
     if (txt->length == 0) {
