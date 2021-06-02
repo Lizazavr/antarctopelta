23f5d3bc77e293a722f8ebfc1cd8d5180ec0a797
Lizazavr <78729103+Lizazavr@users.noreply.github.com>
2021-06-02 05:21:28 +0300

    Update mnwbf.h

--- tests/mnwbf.h
+++ tests/mnwbf.h
@@ -11,13 +11,12 @@ TEST(mnwbf, norm)
 
     append_line(txt, "line 0\n");
     append_line(txt, "line 1\n");
-    append_line(txt, "line 2\n");
 
     std::string output_text = "";
 
-    output_text += "line |0\n";
+    output_text += "\n";
+    output_text += "line 0\n";
     output_text += "line 1\n";
-    output_text += "line 2\n";
 
     testing::internal::CaptureStdout();
 
@@ -30,29 +29,6 @@ TEST(mnwbf, norm)
     EXPECT_STREQ(text.c_str(), output_text.c_str());
 }
 
-TEST(mnwbf, next_line)
-{
-    text txt = create_text();
 
-    append_line(txt, "line 0\n");
-    append_line(txt, "line 1\n");
-    append_line(txt, "line 2\n");
-
-    std::string output_text = "";
-
-    output_text += "line 0\n";
-    output_text += "|line 1\n";
-    output_text += "line 2\n";
-
-    testing::internal::CaptureStdout();
-
-    m(txt, 0, 5);
-    mnwbf(txt);
-    show(txt);
-
-    std::string text = testing::internal::GetCapturedStdout();
-
-    EXPECT_STREQ(text.c_str(), output_text.c_str());
-}
 
 
