7e26b96ab757939f00926057dd362e0e3d661791
Lizazavr <78729103+Lizazavr@users.noreply.github.com>
2021-06-02 16:39:52 +0300

    Update load.cpp

--- app/load.cpp
+++ app/load.cpp
@@ -1,35 +1,33 @@
 #include <stdio.h>
 #include <string.h>
-#include "common.h"
+#include <iostream>
+#include <fstream>
 
-#include "text.h"
-#include "_text.h"
+#include "common.h"
+#include "text/text.h"
 
 /**
  * Загружает содержимое указанного файла
  */
-void load(text txt, char *filename)
+void load(text txt, std::string filename)
 {
-    FILE *f;
+    std::ifstream f;
+    f.open(filename);
 
-    char buf[MAXLINE + 1];
-
-    /* Открываем файл для чтения, контролируя ошибки */
-   // if ((
-        f = fopen(filename, "r");
- /*   ) == NULL) {
-        printf("The file %s cannot be opened\n", filename);
+    if (!f.good()) {
+        std::cerr << "The file " << filename << " cannot be opened\n";
         return;
-    }*/
+    }
 
     /* Удаляем предыдущее содержимое */
     remove_all(txt);
 
-    /* Считываем содержимое строка за строкой */
-    while (fgets(buf, MAXLINE, f)) {
-        buf[strlen(buf) - 1] = '\0';
-        append_line(txt, std::string(buf));
+    std::string buf;
+
+    while (f.good()) {
+        f.getline(&buf[0], MAXLINE);
+        append_line(txt, buf);
     }
 
-    fclose(f);
+    f.close();
 }
