d5bc4b584b1388124fe5086d900694142ae8c977
Lizazavr <78729103+Lizazavr@users.noreply.github.com>
2021-06-02 05:09:25 +0300

    Update m.h

--- tests/m.h
+++ tests/m.h
@@ -8,15 +8,15 @@
 TEST(move, normal)
 {
     text txt = create_text();
-    txt->myList->push_back("line 0\n");
-    txt->myList->push_back("line 1\n");
-    txt->myList->push_back("line 2\n");
+    txt->myList.push_back("line 0\n");
+    txt->myList.push_back("line 1\n");
+    txt->myList.push_back("line 2\n");
 
     txt->cursor->line_num = 0;
     txt->cursor->position = 0;
 
     m(txt, 0, 0);
-    EXPECT_EQ(txt->cursor->line, txt->myList->begin());
+    EXPECT_EQ(txt->cursor->line, txt->myList.begin());
     EXPECT_EQ(txt->cursor->position, 0);
 
     m(txt, 1, 3);
@@ -30,9 +30,9 @@ TEST(move, normal)
 TEST(move, big)
 {
     text txt = create_text();
-    txt->myList->push_back("line 0\n");
-    txt->myList->push_back("line 1\n");
-    txt->myList->push_back("line 2\n");
+    txt->myList.push_back("line 0\n");
+    txt->myList.push_back("line 1\n");
+    txt->myList.push_back("line 2\n");
 
     txt->cursor->line_num = 0;
     txt->cursor->position = 0;
