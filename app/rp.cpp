98571bdb77a629ab07586431eab851550afe51c2
Lizazavr <78729103+Lizazavr@users.noreply.github.com>
2021-06-02 05:35:45 +0300

    Update rp.cpp

--- app/rp.cpp
+++ app/rp.cpp
@@ -1,20 +1,41 @@
-/**
- * ple.cpp -- выводит текст строки после курсора
- *
- * Кузнецова Е.
- */
+/*Программа, удаляющая строчку до той, на которой стоит курсор. Дима Курочкин*/
+
 #include <stdio.h>
 #include <string.h>
 #include "_text.h"
-#include <string>
 
-using namespace std;
+
 void rp(text txt) {
-    list<string>::iterator h = txt->cursor->line;
-    string a(*h);
+    std::list<std::string>::iterator it = txt->myList->begin();
+    int i, line = 0;
+    std::list<std::string>::iterator current = txt->myList->begin();
+/*ищем номер строки, в которой находится курсор*/
+    while(current != txt->cursor->line) {
+        current++;
+        line++;
+        it++;
+    }
+/*Если курсор на первой (нулевой) строке, завершаем работу функции*/
+    if (line == 0)
+        return;
 
-    //Выводим текст от позиции курсора до конца строки
-    a.assign(a, txt->cursor->position, a.length()-txt->cursor->position);//векто
-      printf("%s",a.c_str());
+    it--;
+    txt->myList->erase(it--);
+
+    if (line == 1) {
+        current = current++;
+        txt->myList->begin() = current;
+        return;
+    }
+    for (i = 0; i < line-1; i++) {
+        if (i == line-2) {
+            current++ = (current++)++;
+            current = current++;
+            current-- = (current--)--;
+            return;
+        }
+        current = current++;
+    }
+    return;
 }
 
