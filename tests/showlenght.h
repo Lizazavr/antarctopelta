331e708bbb1af459fc792d81e9c2cc827bc11ae6
Lizazavr <78729103+Lizazavr@users.noreply.github.com>
2021-06-07 09:18:14 +0300

    Update showlenght.h

--- tests/showlenght.h
+++ tests/showlenght.h
@@ -6,18 +6,16 @@
 TEST(showodd, norm)
 {
     text txt = create_text();
-
-    append_line(txt, "line \n");
-    append_line(txt, "line 2\n");
-    append_line(txt, "line 22\n");
+    append_line(txt, "lool lol\n");
+    append_line(txt, "lool lool\n");
+    append_line(txt, "lool loool\n");
+    append_line(txt, "lool looool\n");
 
     std::string output_text = "";
 
-    output_text += "line \n";
-    output_text += "line 22\n";
-
+    output_text += "lool lol\n";
+    output_text += "lool loool\n";
     testing::internal::CaptureStdout();
-
     m(txt, 0, 1);
     showlenght(txt);
     std::string text = testing::internal::GetCapturedStdout();
@@ -31,18 +29,9 @@ TEST(showodd, zero)
      append_line(txt, "\n");
      std::string output_text = "";
      output_text += "\n";
-
-     std::string output_text = "";
-
-     output_text += "\n";
-
      testing::internal::CaptureStdout();
-
      m(txt, 0, 1);
      showlenght(txt);
-
      std::string text = testing::internal::GetCapturedStdout();
-
      EXPECT_STREQ(text.c_str(), output_text.c_str());
 }
-
