3e6c6faed456e63fa48391ba51b525c782a3174b
Lizazavr <78729103+Lizazavr@users.noreply.github.com>
2021-06-07 09:04:03 +0300

    Update m.h

--- tests/m.h
+++ tests/m.h
@@ -5,12 +5,12 @@
 #include "text.h"
 #include "_text.h"
 
-TEST(move, normal)
+TEST(m, normal)
 {
     text txt = create_text();
-    txt->myList.push_back("line 0\n");
-    txt->myList.push_back("line 1\n");
-    txt->myList.push_back("line 2\n");
+    txt->myList.push_back("0 lool\n");
+    txt->myList.push_back("1 lool\n");
+    txt->myList.push_back("2 lool\n");
 
     txt->cursor->line_num = 0;
     txt->cursor->position = 0;
@@ -19,32 +19,32 @@ TEST(move, normal)
     EXPECT_EQ(txt->cursor->line, txt->myList.begin());
     EXPECT_EQ(txt->cursor->position, 0);
 
-    m(txt, 1, 3);
-    EXPECT_EQ(txt->cursor->line_num, 1);
-    EXPECT_EQ(txt->cursor->position, 3);
+    m(txt, 2, 1);
+    EXPECT_EQ(txt->cursor->line_num, 2);
+    EXPECT_EQ(txt->cursor->position, 1);
 
     free(txt);
 }
 
 
-TEST(move, big)
+TEST(m, big)
 {
     text txt = create_text();
-    txt->myList.push_back("line 0\n");
-    txt->myList.push_back("line 1\n");
-    txt->myList.push_back("line 2\n");
+    txt->myList.push_back("0 lool\n");
+    txt->myList.push_back("1 lool\n");
+    txt->myList.push_back("2 lool\n");
 
     txt->cursor->line_num = 0;
     txt->cursor->position = 0;
 
 
-    m(txt, 100, 0);
+    m(txt, 1000, 0);
     EXPECT_EQ(txt->cursor->line_num, 0);
     EXPECT_EQ(txt->cursor->position, 0);
-    m(txt, 100, 100);
+    m(txt, 1000, 1000);
     EXPECT_EQ(txt->cursor->line_num, 0);
     EXPECT_EQ(txt->cursor->position, 0);
-    m(txt, 0, 100);
+    m(txt, 0, 1000);
     EXPECT_EQ(txt->cursor->line_num, 0);
     EXPECT_EQ(txt->cursor->position, 7);
     free(txt);
