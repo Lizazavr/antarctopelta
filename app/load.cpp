de51c69c72b8c7c2bc1e3347a0375dea64f23201
Lizazavr <78729103+Lizazavr@users.noreply.github.com>
2021-06-07 10:39:17 +0300

    Update load.cpp

--- app/load.cpp
+++ app/load.cpp
@@ -25,7 +25,7 @@ void load(text txt, char *filename)
 
     /* Считываем содержимое строка за строкой */
     while (fgets(buf, MAXLINE, f)) {
-        buf[strlen(buf) - 1] = '\0';
+        buf[strlen(buf) - 1] = '\0'
         append_line(txt, std::string(buf));
     }
 
