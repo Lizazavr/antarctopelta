821e2c42e650b74191ba72e00986d828b5a8fa75
Lizazavr <78729103+Lizazavr@users.noreply.github.com>
2021-06-07 08:44:23 +0300

    Update create_text.cpp

--- app/create_text.cpp
+++ app/create_text.cpp
@@ -22,7 +22,7 @@ text create_text()
         exit(EXIT_FAILURE);
     }
 
-    txt.myList = std::list<std::string>();
+    txt->myList = std::list<std::string>();
 
 
     txt->cursor->line = txt->myList.end();
